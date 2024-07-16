31af8b8a616d7d369a92211c912360854d44e5db
Maurício Vieira Silva <mauricio@berani.com.br>
2024-07-16 15:05:43 -0300

    refactor: ajuste para evitar repetição de código

--- app/Traits/HandlesResponses.php
+++ app/Traits/HandlesResponses.php
@@ -6,28 +6,33 @@ use Illuminate\Http\Response;
 
 trait HandlesResponses
 {
+    protected function handleResponse(int $statusCode)
+    {
+        return response()->noContent($statusCode);
+    }
+
     protected function handleCreatedResponse()
     {
-        return response()->noContent(Response::HTTP_CREATED);
+        return $this->handleResponse(Response::HTTP_CREATED);
     }
 
     protected function handleNoContentResponse()
     {
-        return response()->noContent(Response::HTTP_NO_CONTENT);
+        return $this->handleResponse(Response::HTTP_NO_CONTENT);
     }
 
     protected function handleUnprocessableEntityResponse()
     {
-        return response()->noContent(Response::HTTP_UNPROCESSABLE_ENTITY);
+        return $this->handleResponse(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     protected function handleBadRequestResponse()
     {
-        return response()->noContent(Response::HTTP_BAD_REQUEST);
+        return $this->handleResponse(Response::HTTP_BAD_REQUEST);
     }
 
-    protected function handleInternalserverErrorResponse()
+    protected function handleInternalServerErrorResponse()
     {
-        return response()->noContent(Response::HTTP_INTERNAL_SERVER_ERROR);
+        return $this->handleResponse(Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
