0522b00e974b8af23445d24e35a4c5a1eadb620b
Maurício Vieira Silva <mauricio@berani.com.br>
2024-07-16 14:44:03 -0300

    feat: criação das rotas

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\StatisticsController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/transactions', [TransactionController::class, 'store']);
-Route::get('/statistics', [StatisticsController::class, 'index']);
-Route::delete('/transactions', [TransactionController::class, 'destroy']);
+Route::post('/transactions', [TransactionController::class, 'create']);
+Route::get('/statistics', [StatisticsController::class, 'getLast']);
+Route::delete('/transactions', [TransactionController::class, 'delete']);
